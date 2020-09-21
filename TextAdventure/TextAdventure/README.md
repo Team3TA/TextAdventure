86c18bec31fb62220f6363bb95d7984de50b11fc
Fraleym7700 <54638784+Fraleym7700@users.noreply.github.com>
2020-09-21 15:55:01 -0400

    Add files via upload

---
+++ TextAdventure/TextAdventure/README.md
@@ -0,0 +1,2 @@
+# TextAdventure
+Group 3 Text Adventure Project,
---
+++ TextAdventure/TextAdventure/TextAdventure/ConsoleUI/BuildPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAdventureLib;
+
+namespace ConsoleUI
+{
+    public class BuildPlayer
+    {
+        public static void BuildAPlayer(List<Player> inputList)
+        {
+            
+            Player thisPlayer = new Player();
+            
+            Console.Write("Enter your player name --> ");
+            thisPlayer.Name = Console.ReadLine();
+            Console.Write("Enter your password --> ");
+            thisPlayer.Password = Console.ReadLine();
+            Console.Write("Choose a class(Warrior, Rogue, Mage, Paladin) --> ");
+            thisPlayer.Class = Console.ReadLine();
+            Console.Write("Choose a race(Human, Halfling, Elf, Kobold) --> ");
+            thisPlayer.Race = Console.ReadLine();
+
+            inputList.Add(thisPlayer);
+            
+        }
+    }
+}
