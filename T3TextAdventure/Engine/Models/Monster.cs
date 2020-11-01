cf42fe80ba097d2d5201d1a055861b9a1bcf7af0
Fraleym7700 <54638784+Fraleym7700@users.noreply.github.com>
2020-11-01 16:24:04 -0500

    WPFVersion
    Created WPF Version of teh Text Adventure

---
+++ T3TextAdventure/Engine/Models/Monster.cs
@@ -0,0 +1,26 @@
+﻿namespace Engine.Models
+{
+    public class Monster : LivingEntity
+    {
+        public string ImageName { get; set; }
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public int RewardExperiencePoints { get; private set; }
+
+        public Monster(string name, string imageName,
+                       int maximumHitPoints, int hitPoints,
+                       int minimumDamage, int maxmumDamage,
+                       int rewardExperiencePoints, int rewardGold)
+        {
+            Name = name;
+            ImageName = $"/Engine;component/Images/Monsters/{imageName}";
+            MaximumHitPoints = maximumHitPoints;
+            CurrentHitPoints = hitPoints;
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maxmumDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            Gold = rewardGold;
+        }
+    }
+}
